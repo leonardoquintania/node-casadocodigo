d68e3cfb78126bf372319c7037cbb9f50a2df8cf
Leonardo Dos Santos Quintania <leonardo.quintania@sp01.local>
2020-01-23 04:26:33 -0300

    Arquivos iniciais feitos do node

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const servidor = http.createServer((request, response) => {
+    response.end(` 
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Casa do Código </h1>
+            </body> 
+        </html>
+    `)
+});
+
+servidor.listen(3000);
